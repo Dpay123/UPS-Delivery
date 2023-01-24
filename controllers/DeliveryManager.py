1c59fe6a97df04f7d71ad84f61a19e57a0f090c6
Dpay <payerdom@my.uri.edu>
2023-01-24 12:19:22 -0500

    Add priority cargo to truck. Cleaned up manual adding of packages

--- controllers/DeliveryManager.py
+++ controllers/DeliveryManager.py
@@ -1,7 +1,6 @@
 from models.Graph import Graph
 from models.MyHashTable import MyHashTable
 from models.Truck import Truck
-import datetime
 
 # This is the class that handles the main functionality of the program
 # Upon initialization, data is read from the .CSV and loaded into the manager
@@ -19,8 +18,6 @@ class DeliveryManager:
         # create two trucks
         self.truck1 = Truck("Truck 1")
         self.truck2 = Truck("Truck 2")
-        # keep track of time
-        self.time = datetime.timedelta(hours=8, minutes=0, seconds=0)
 
     # Remove a package from the hub and add to the specified truck
     def transfer_package_to_truck(self, package_id, truck):
@@ -34,39 +31,14 @@ class DeliveryManager:
     # Load truck 1 and truck 2
     def load_first_trucks(self):
         # manual load packages to truck 1
-        self.transfer_package_to_truck(1, self.truck1)
-        self.transfer_package_to_truck(2, self.truck1)
-        self.transfer_package_to_truck(5, self.truck1)
-        self.transfer_package_to_truck(11, self.truck1)
-        self.transfer_package_to_truck(12, self.truck1)
-        self.transfer_package_to_truck(13, self.truck1)
-        self.transfer_package_to_truck(14, self.truck1)
-        self.transfer_package_to_truck(15, self.truck1)
-        self.transfer_package_to_truck(16, self.truck1)
-        self.transfer_package_to_truck(19, self.truck1)
-        self.transfer_package_to_truck(20, self.truck1)
-        self.transfer_package_to_truck(21, self.truck1)
-        self.transfer_package_to_truck(22, self.truck1)
-        self.transfer_package_to_truck(23, self.truck1)
-        self.transfer_package_to_truck(33, self.truck1)
-        self.transfer_package_to_truck(40, self.truck1)
+        truck1 = [1, 2, 5, 11, 12, 13, 14, 15, 16, 19, 20, 21, 22, 23, 33, 40]
+        for id in truck1:
+            self.transfer_package_to_truck(id, self.truck1)
+
         # manual load packages to truck 2
-        self.transfer_package_to_truck(3, self.truck2)
-        self.transfer_package_to_truck(4, self.truck2)
-        self.transfer_package_to_truck(6, self.truck2)
-        self.transfer_package_to_truck(17, self.truck2)
-        self.transfer_package_to_truck(18, self.truck2)
-        self.transfer_package_to_truck(24, self.truck2)
-        self.transfer_package_to_truck(25, self.truck2)
-        self.transfer_package_to_truck(26, self.truck2)
-        self.transfer_package_to_truck(29, self.truck2)
-        self.transfer_package_to_truck(30, self.truck2)
-        self.transfer_package_to_truck(31, self.truck2)
-        self.transfer_package_to_truck(32, self.truck2)
-        self.transfer_package_to_truck(34, self.truck2)
-        self.transfer_package_to_truck(36, self.truck2)
-        self.transfer_package_to_truck(37, self.truck2)
-        self.transfer_package_to_truck(38, self.truck2)
+        truck2 = [3, 4, 6, 17, 18, 24, 25, 26, 29, 30, 31, 32, 34, 36, 37, 38]
+        for id in truck2:
+            self.transfer_package_to_truck(id, self.truck2)
 
     # when either truck 1 or truck 2 returns, load the remaining packages onto that truck
     def load_third_truck(self, truck):
@@ -74,6 +46,7 @@ class DeliveryManager:
         for i in range(40):
             if self.hub.table[i]:
                 self.transfer_package_to_truck(i+1, truck)
+        truck.name = "Truck 3"
 
     def truck_deliver_packages(self, truck):
         # TODO
@@ -83,6 +56,5 @@ class DeliveryManager:
         self.hub.print()
         self.truck1.print()
         self.truck2.print()
-        print("Time: ", self.time)
         print("Miles Traveled: %d" % self.mileage)
         print()
--- models/Truck.py
+++ models/Truck.py
@@ -6,18 +6,28 @@ class Truck:
         self.name = name
         self.location = "4001 South 700 East"
         # cargo to hold packages
+        self.priority_cargo = []
         self.cargo = []
 
+    # check package priority and load
     def load_package(self, package):
-        self.cargo.append(package)
+        if package.deadline != "EOD":
+            self.priority_cargo.append(package)
+        else :
+            self.cargo.append(package)
 
     def unload_package(self, address):
         for package in self.cargo:
             if package.address == self.location:
                 self.cargo.remove(package)
 
+    # prints an overview of the truck status, including name, location, packages held
+    # then prints priority packages held
+    # then prints remaining packages held
     def print(self):
-        print("%s | Location: %s | Packages Held: %s" % (self.name, self.location, len(self.cargo)))
+        print("%s | Location: %s | Packages Held: %s" % (self.name, self.location, len(self.cargo) + len(self.priority_cargo)))
+        for p in range(len(self.priority_cargo)):
+            print("---priority package", self.priority_cargo[p])
         for p in range(len(self.cargo)):
             print("---package", self.cargo[p])
         print()
