1998d18c34ae3faa7285f97fdde3f3e2aab79cbc
Dpay <payerdom@my.uri.edu>
2023-01-27 14:08:26 -0500

    Fixed bug

--- main.py
+++ main.py
@@ -6,7 +6,13 @@ if __name__ == '__main__':
     # create a DeliveryManager
     dm = DeliveryManager()
 
-    # implement functionality using manager
+    # load trucks
     dm.load_first_trucks()
+
+    # deliver
     dm.truck_deliver_packages(dm.truck1)
+    dm.truck_deliver_packages(dm.truck2)
+
+    # test final truck delivery
+
     dm.status()
